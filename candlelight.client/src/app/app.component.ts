0c2b6a7737f958239fb02c91d52f2e755cf6950d
Jakub Klimczak <kubaklim2001@gmail.com>
2024-11-04 20:41:16 +0100

    Fix styling (#5)
    * Progress 07.09.2024
    
    * Progress 07.09.2024
    
    * ES2022 modules -> ESNext modules
    
    * Battle with styling

--- candlelight.client/src/app/app.component.ts
+++ candlelight.client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   constructor(private http: HttpClient) { }
