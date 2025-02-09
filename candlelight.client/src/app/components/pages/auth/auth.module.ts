593210cff4c7c4b2bda7b4d7293972d1687ca643
jakubklimczak <kubaklim2001@gmail.com>
2025-02-09 21:20:34 +0100

    Frontend - register form css

--- candlelight.client/src/app/components/pages/auth/auth.module.ts
+++ candlelight.client/src/app/components/pages/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { RegistrationPageComponent } from './registration-page/registration-page
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
 })
 export class AuthModule { }
