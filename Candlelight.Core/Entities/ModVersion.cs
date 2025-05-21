307be8210ae1a494df2665bac8e8ec83db85a91a
jakubklimczak <kubaklim2001@gmail.com>
2025-05-21 13:04:38 +0200

    Migration

--- Candlelight.Core/Entities/ModVersion.cs
+++ Candlelight.Core/Entities/ModVersion.cs
@@ -6,9 +6,9 @@ public class ModVersion : BaseEntity
     public string Version { get; set; } = default!;
     public string Changelog { get; set; } = default!;
     public string FileUrl { get; set; } = default!;
-    public List<string> SupportedVersions { get; set; } = default!;
+    public List<string>? SupportedVersions { get; set; } = default!;
     public long FileSizeBytes { get; set; } = default!;
-    public List<Guid> Dependencies { get; set; } = default!;
+    public List<Guid>? Dependencies { get; set; } = default!;
     public int DownloadCount { get; set; } = default!;
     public string FileType { get; set; } = default!;
 
--- Candlelight.Infrastructure/Migrations/20250521110230_ReviewsAndFavourites.Designer.cs
+++ Candlelight.Infrastructure/Migrations/20250521110230_ReviewsAndFavourites.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Candlelight.Infrastructure.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Candlelight.Infrastructure.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250521110230_ReviewsAndFavourites")]
+    partial class ReviewsAndFavourites
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
